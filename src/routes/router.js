5d894460915783ab37ae30af489ed784d590fd15
KengKaK <suppadej3@gmail.com>
2025-04-21 00:54:08 +0700

    get & post

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
+const product = [{name:"toyota",price:"1,000,000",description:"taotoyota",image:"image/toyota.jpg"},
+    {name:"honda",price:"1,200,000",description:"taohonda",image:"image/honda.png"},
+    {name:"isuzu",price:"900,000",description:"taohonda",image:"image/isuzu.png"}]
 
 router.get('/',(req,res)=>{
-    const product = [{name:"toyota",price:"1,000,000",description:"taotoyota",image:"image/toyota.jpg"},
-        {name:"honda",price:"1,200,000",description:"taohonda",image:"image/honda.png"},
-        {name:"isuzu",price:"900,000",description:"taohonda",image:"image/isuzu.png"}]
     res.render('index.ejs',{products:product})
+})
+
+router.get('/addform',(req,res)=>{
+    res.render('add_product.ejs')
+})
+
+router.get('/edit',(req,res)=>{
+    res.render('edit_product.ejs',{products:product})
+})
+
+router.post('/insert',(req,res)=>{
+    console.log(req.body);
+    res.render('add_product.ejs')
 })
 
 module.exports = router
