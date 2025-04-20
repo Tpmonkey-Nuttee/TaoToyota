6fcefa1016ab85d02225e98a833385792b3c5455
KengKaK <suppadej3@gmail.com>
2025-04-20 21:26:06 +0700

    change router

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,29 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const path = require('path')
-const index_page = path.join(__dirname,'../index.html')
-const products_page1 = path.join(__dirname,'../products1.html')
-const products_page2 = path.join(__dirname,'../products2.html')
 
-router.get('/', (req, res) => {
-    res.status(200)
-    res.type('text/html')
-    res.sendFile(index_page)
-})
-  
-router.get('/products/:id',(req,res) => {
-    res.status(200)
-    res.type('text/html')
-    const products_id = req.params.id
-    if(products_id === "1"){
-        res.sendFile(products_page1)
-    }else if(products_id === "2"){
-        res.sendFile(products_page2)
-    }else{
-        res.redirect('/')
-    }
-
-})
 
 module.exports = router
   
