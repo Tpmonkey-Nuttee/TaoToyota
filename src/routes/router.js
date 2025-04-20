101ec1a5b9a3143488daa4dc87c2d51cede5dc60
KengKaK <suppadej3@gmail.com>
2025-04-20 22:52:34 +0700

    change main page to ejs

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,7 +3,10 @@ const router = express.Router()
 const path = require('path')
 
 router.get('/',(req,res)=>{
-    res.render('index.ejs')
+    const product = [{name:"toyota",price:"1,000,000",description:"taotoyota",image:"image/toyota.jpg"},
+        {name:"honda",price:"1,200,000",description:"taohonda",image:"image/honda.png"},
+        {name:"isuzu",price:"900,000",description:"taohonda",image:"image/isuzu.png"}]
+    res.render('index.ejs',{products:product})
 })
 
 module.exports = router
